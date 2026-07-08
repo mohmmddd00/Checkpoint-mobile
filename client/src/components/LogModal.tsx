f8868cc2d7cc5007b29a3edfabfdbb7d3d68d49d
Mohammad Mustafa <163987556+mohmmddd00@users.noreply.github.com>
2026-07-08 18:29:19 +0500

    Update LogModal.tsx

--- client/src/components/LogModal.tsx
+++ client/src/components/LogModal.tsx
@@ -245,7 +245,7 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: "#380B14",
     borderRadius: 12,
-    padding: 28,
+    padding: 26,
   },
   header: {
     flexDirection: "row",
